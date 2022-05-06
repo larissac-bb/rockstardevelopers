b4f351e46120e9e201502c98e90cee3d58ec93cb
larissax <larissa.christ1@hs-augsburg.de>
2022-05-06 14:16:33 +0200

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import Navigation from './Navigation';
+import MainPage from './MainPage';
 
 function App() {
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products?limit=10')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+  }) 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <MainPage />
     </div>
   );
 }
---
+++ src/MainPage.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './MainPage.css';
+
+export default function MainPage() {
+    return (
+        <div className='content'>
+            halihalo
+        </div>
+    )
+}
